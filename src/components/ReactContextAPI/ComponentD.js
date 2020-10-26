c52546507ebcdf8506b36a9d653f7d252791b231
Chaitanya <chaitu.vsh@gmail.com>
2020-10-26 17:13:39 +0530

    React Context API with ContextTypes

--- src/components/ReactContextAPI/ComponentD.js
+++ src/components/ReactContextAPI/ComponentD.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import ComponentF from "./ComponentF";
+import UserContext from "./UserContext";
 
 class ComponentD extends Component {
   render() {
     return (
       <div className="container">
         <h1>This is the Component D</h1>
+        <h2>This is the context: {this.context}</h2>
         <ComponentF />
       </div>
     );
   }
 }
-
+ComponentD.contextType = UserContext;
 export default ComponentD;
